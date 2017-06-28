cdc36b9294cdb2e144ee792622c945f2e4138a6e
Lauren Leibowitz <lleibowitz@carecloud.com>
2017-06-27 21:23:44 -0400

    Add Like functionality

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class ShowList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoList: this.props.list
+      todoList: this.props.list,
+      likes: 0
     }
   this.handleDelete = this.handleDelete.bind(this)
+  this.addLike = this.addLike.bind(this)
     }
   
   handleDelete(event) {
@@ -19,30 +21,32 @@ class ShowList extends Component {
   let array= this.state.todoList
   let id = event.target.id
   array.splice(id,1)
-  console.log(array[id])
-  console.log(id)
-  console.log(array)
   this.setState({todoList: array });
 }
 
 addLike(event) {
   event.preventDefault()
-  // 
-  // let like = 0
-  // like += 1
+  let array= this.state.todoList
+  let id= event.target.id
+  let item= array[id]
+  item.like++
+  let like= this.state.todoList
+  this.setState({toList: like})
+  
 }
   
   render() { 
     
     const createTasks = (item,index) =>  {
       let date = (new Date()).toDateString()
-      let like = 0
-      item.index = index
-      console.log(item)
+      item.index= index
+      let like = item.like
+      // console.log(like)
       return <li className="item" id={index} key={index} ><span id="date">{date}</span>:{' '}{item.name}{' '}
       <button className="deleteButton" type="button" id={item.index} onClick={this.handleDelete}>Delete</button>
-      <button className="likeButton" type="button"  id={item.index}onClick={this.addLike}>Like</button>
-      <p>{like}</p></li>
+      <button className="likeButton" type="button" id={item.index} onClick={this.addLike}>Like</button>
+      <p>{like}</p>
+      </li>
 }
 
   let listItems = this.state.todoList.map(createTasks);
@@ -80,7 +84,7 @@ class ListForm extends Component {
       let item = event.target.name.value 
       this.setState({name: item})
       let index = Date.now()
-      this.state.list.push({name: item, key: index})
+      this.state.list.push({name: item, key: index, like: 0})
     //bug:  form list does not reset when submitted event.target.reset()   - event.target.name.value.reset()
     }
     
